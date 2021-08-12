72fbb6dc3b93f19c60499767744055c998e52e0c
MiketoCoding <le.michael.v@gmail.com>
2021-08-12 14:33:00 -0400

    Add header and logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import { Container, Navbar } from 'react-bootstrap'
+import logo from '../assets/orange-wellness-logo.png'
 
 function Footer() {
   return (
     <Navbar fixed='bottom' bg='primary'>
-      <Container>poop</Container>
+      <Container>
+        <img
+          src={logo}
+          alt="Orange Wellness Logo"
+          className="d-inline-block align-top"
+          width="100px"
+        />
+      </Container>
     </Navbar>
   )
 }
