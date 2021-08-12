2b284526b5cd4d1400ad2781160058e07b505457
MiketoCoding <le.michael.v@gmail.com>
2021-08-12 19:03:06 -0400

    Add links to articles
    Links to articles added to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,7 @@ import {
   FormControl,
   Jumbotron,
 } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import image from '../assets/image.svg'
 import hero from '../assets/hero-image.jpeg'
 import SearchForm from '../components/SearchForm'
@@ -27,9 +28,10 @@ export default function Home({ setResults, loading, setLoading }) {
       </Jumbotron>
       <Row>
         <Col>
+          <a href="https://thehill.com/blogs/congress-blog/healthcare/567478-how-athletes-taking-a-stand-on-mental-health-could-help-the?rl=1" target="_blank" rel="noreferrer">
           <Card>
             {/* <Card.Img variant='top' src={image} thumbnail /> */}
-            <Card.Img variant="top" src="https://thehill.com/sites/default/files/styles/thumb_small_article/public/bilessimone_080121getty.jpg?itok=W8rU_5ew" />
+            <Card.Img variant="top" src="https://thehill.com/sites/default/files/styles/thumb_small_article/public/bilessimone_080121getty.jpg?itok=W8rU_5ew" target="_blank" />
             <Card.Body>
               <Card.Title>How athletes taking a stand on mental health could help the nation's mothers
               </Card.Title>
@@ -38,8 +40,10 @@ export default function Home({ setResults, loading, setLoading }) {
               </Card.Text>
             </Card.Body>
           </Card>
+          </a>
         </Col>
         <Col>
+          <a href="https://www.nytimes.com/2019/08/29/smarter-living/how-to-manage-mental-illness-at-work.html" target="_blank" rel="noreferrer">
           <Card>
             {/* <Card.Img variant='top' src={image} text-muted /> */}
             <Card.Img variant="top" src="https://static01.nyt.com/images/2019/09/09/smarter-living/09sl-mentalillness/00sl-mentalillness-superJumbo.jpg?quality=90&auto=webp" />
@@ -50,8 +54,10 @@ export default function Home({ setResults, loading, setLoading }) {
               </Card.Text>
             </Card.Body>
           </Card>
+          </a>
         </Col>
         <Col>
+          <a href="https://www.nytimes.com/2019/01/29/well/mind/fighting-the-stigma-of-mental-illness-through-music.html" target="_blank" rel="noreferrer">
           <Card>
             {/* <Card.Img variant='top' src={image} text-muted /> */}
             <Card.Img variant="top" src="https://static01.nyt.com/images/2019/01/22/well/family/music-braunstein/music-braunstein-jumbo.jpg?quality=90&auto=webp"/>
@@ -62,8 +68,10 @@ export default function Home({ setResults, loading, setLoading }) {
               </Card.Text>
             </Card.Body>
           </Card>
+          </a>
         </Col>
         <Col>
+          <a href="https://www.nytimes.com/2020/08/25/well/mind/black-mental-health.html" target="_blank" rel="noreferrer">
           <Card>
             {/* <Card.Img variant='top' src={image} text-muted /> */}
             <Card.Img variant="top" src="https://static01.nyt.com/images/2020/02/10/well/00well-stigma/00well-stigma-jumbo.jpg?quality=90&auto=webp"/>
@@ -75,6 +83,7 @@ export default function Home({ setResults, loading, setLoading }) {
               </Card.Text>
             </Card.Body>
           </Card>
+          </a>
         </Col>
       </Row>
 
